c30dc49ade4203735a03c90f506c8ef4ba178655
xavier skip <xavierskip@gmail.com>
2018-09-20 17:32:10 +0800

    add arg "-c"

--- oa/__init__.py
+++ oa/__init__.py
@@ -1,17 +1,28 @@
 from oa import *
-from mylog import spiderlog, maillog, ready4log
+from logger import logger_configure
 import ConfigParser
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", help="config file",
+                    type=str)
+args = parser.parse_args()
 
 
 def start_config():
     ini = ConfigParser.ConfigParser()
-    ini.read(CONFIG['INI'])
+    if args.c != None:
+        # print(args.c, os.path.abspath(args.c))
+        ini.read(os.path.abspath(args.c))
+    else:
+        ini.read(CONFIG['INI'])
     overwriteconfig_4logger(ini)  # 
     return ini
 
 def overwriteconfig_4logger(ini):
     for k, v in ini.items('config'):
         CONFIG[k.upper()] = v.decode('utf-8')
-    ready4log()
+    logger_configure(ini)
 
-OA_ini = start_config()
+OAini = start_config()
